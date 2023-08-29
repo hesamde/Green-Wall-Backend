4c82a266ba6c605cf45baa5d5d117f05bba64006
hesam dehghanmoshtaghin <hdm.hesam@gmail.com>
2023-08-29 21:06:04 +0200

    🏗️config clouldinary

--- app.js
+++ app.js
@@ -21,6 +21,11 @@ app.use("/api", indexRoutes);
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
 
+// const productRoutes = require("./routes/product.routes");
+// app.use("/product", productRoutes);
+
+
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,40 @@
 const express = require("express");
 const router = express.Router();
 
+const User = require("../models/User.model");
+const fileUploader = require("../config/cloudinary.config");
+
+const { isAuthenticated } = require("../middleware/jwt.middleware.js");
+
 router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
+
+router.post("/upload",fileUploader.single("image"), (req, res)=>{
+  if (!req.file) {
+    next(new Error("No file uploaded!"));
+    return;
+  }
+
+  res.json({ image: req.file.path });
+})
+
+router.get("/users", isAuthenticated, (req, res) => {
+  console.log('payload', req.payload)
+  res.status(200).json(req.payload)
+
+})
+
+router.put("/users", (req, res) => {
+  const {_id, image } = req.body;
+
+  User.findByIdAndUpdate(_id, { image }, {new: true})
+    .then(updatedUser => {
+      const {_id, name,email,image  } = updatedUser
+      res.json({ updatedUser: {_id, name,email,image} })
+    })
+    .catch(err => console.error(err))
+
+})
 
 module.exports = router;
