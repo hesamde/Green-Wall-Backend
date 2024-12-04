1933abd3d1659650d5970c48c65685eae7425143
hesam dehghanmoshtaghin <hdm.hesam@gmail.com>
2024-12-04 17:06:42 +0100

    ignore cors

--- config/index.js
+++ config/index.js
@@ -22,11 +22,7 @@ module.exports = (app) => {
   app.set("trust proxy", 1);
 
   // make sure the deployment send the request to back end
-  app.use(
-    cors({
-      origin: [FRONTEND_URL],
-    })
-  );
+  app.use(cors());
 
   // In development environment the app logs
   app.use(logger("dev"));
